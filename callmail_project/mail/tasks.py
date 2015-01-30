dbf6f53e804673cb4d2eb85ec74d8a8e01f8cd1e
Mohammed Mac Air <mohammad@labelag.com>
2015-01-30 21:07:48 +0300

    add schedular task

--- callmail_project/mail/tasks.py
+++ callmail_project/mail/tasks.py
@@ -2,27 +2,12 @@ from datetime import timedelta
 
 from celery import task
 
-from mail_fetch import FetchEmail
-
+from django_mailbox.models import Mailbox
 
 @task
 def email_cron():
-    server = FetchEmail()
-    server.login()
-    message_id = server.get_email_id()
-
-    if message_id:
-        server.fetch_emails(message_id)
-        server.flush_emails()
+    mailboxes = Mailbox.active_mailboxes.get('callnmail')
+    mailboxes.get_new_mail()
 
 
 
-
-CELERYBEAT_SCHEDULE = {
-    'email_cron': {
-        'task': 'tasks.email_cron',
-        'schedule': timedelta(seconds=180),
-    },
-}
-
-CELERY_TIMEZONE = 'UTC'
