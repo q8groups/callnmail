ec028bb4cf507e1cfb1a39e1a4b62983dba0f7a9
Mohammed Mac Air <mohammad@labelag.com>
2015-02-26 19:56:39 +0300

    adding default json

--- callmail_project/api/views.py
+++ callmail_project/api/views.py
@@ -75,8 +75,8 @@ class RegistrationView(generics.CreateAPIView):
                 user.first_name = first_name
                 user.last_name = last_name
                 user.save()
-                profile = UserProfile.objects.get_or_create(user=user, avatar=avatar, country=country.upper())
-                profile.save()
+                #profile = UserProfile.objects.get_or_create(user=user, avatar=avatar, country=country.upper())
+                #profile.save()
                 random_number = generate_random_number()
                 TokenValidation.objects.create(user=user, secret_token=random_number)
                 send_sms(phone_number, 'Your activation code is ' + str(random_number))
