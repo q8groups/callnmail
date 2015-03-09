ad012bcb829c3a6767f7883fc70f2e141b1e26a9
Mohammed Mac Air <mohammad@labelag.com>
2015-03-09 22:11:46 +0300

    removing print

--- callmail_project/mail/admin.py
+++ callmail_project/mail/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from .models import Mail, MailAttachment, MailForward, ForgotPasswordToken, AccountActivation, ContactUs
 
-admin.site.register(Mail)
-admin.site.register(MailAttachment)
+#admin.site.register(Mail)
+#admin.site.register(MailAttachment)
 admin.site.register(MailForward)
-admin.site.register(ForgotPasswordToken)
-admin.site.register(AccountActivation)
+#admin.site.register(ForgotPasswordToken)
+#admin.site.register(AccountActivation)
 admin.site.register(ContactUs)
