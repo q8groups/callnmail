9dd08541439527c6c1c04fd85d1b80e66d5ab243
Mohammed Mac Air <mohammad@labelag.com>
2015-04-24 20:42:29 +0300

    adding new redis

--- callmail_project/advertisement/models.py
+++ callmail_project/advertisement/models.py
@@ -48,6 +48,7 @@ class Banner(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to="banners/")
     priority = models.PositiveIntegerField(default=0)
+    link = models.URLField()
 
 
 
