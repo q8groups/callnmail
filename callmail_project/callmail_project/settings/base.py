f612ed258b48337d249ce754d04fdb796c69ce1c
q80 <mohammad@labelag.com>
2015-05-02 20:23:01 +0300

    adding few changes

--- callmail_project/callmail_project/settings/base.py
+++ callmail_project/callmail_project/settings/base.py
@@ -321,3 +321,10 @@ AWS_STORAGE_BUCKET_NAME='callnmail'
 AWS_S3_SECURE_URLS = False
 AWS_QUERYSTRING_AUTH = False
 
+TINYMCE_DEFAULT_CONFIG = {
+    'plugins': "table,spellchecker,paste,searchreplace",
+    'theme': "advanced",
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 10,
+}
+
