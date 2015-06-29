581b12f5abfc2fa6b6fdd28822c33854955d6c51
q80 <mohammad@labelag.com>
2015-06-29 22:27:03 +0300

    fetchmail celery instance

--- callmail_project/aboutus/models.py
+++ callmail_project/aboutus/models.py
@@ -4,7 +4,7 @@ from tinymce import models as tinymce_models
 
 class StaticContents(models.Model):
     title = models.CharField(max_length=255)
-    body = tinymce_models.HTMLField()
+    body = models.TextField()
     active = models.BooleanField(default=False)
 
     def __unicode__(self):
