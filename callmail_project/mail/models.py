f2503e822699069d05f2f6dcfbbc1f0fc2b24837
Mohammed Alhajeri <malhajri@ooredoo.com.kw>
2015-12-02 12:34:03 +0300

    fixing signals

--- callmail_project/mail/models.py
+++ callmail_project/mail/models.py
@@ -2,9 +2,13 @@ import os
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import EmailMultiAlternatives
 
+from advertisement.models import UserProfile, Advertisement
 
-
+from rest_framework.authtoken.models import Token
 
 
 
@@ -67,3 +71,15 @@ class ContactUs(models.Model):
 
 
 
+@receiver(post_save, sender=ContactUs)
+def SendEmailContactUs(sender, instance=None, created=False, **kwargs):
+    if created:
+        msg = EmailMultiAlternatives(instance.subject, instance.message, instance.contact_email, ['contact@callnmail.com',])
+        msg.send()
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
--- callmail_project/mail/signals.py
+++ callmail_project/mail/signals.py
@@ -3,22 +3,13 @@ from django.dispatch import receiver
 from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth.models import User
 
-from advertisement.models import UserProfile, Advertisement
+from advertisement.models import UserProfile
 
 
 from rest_framework.authtoken.models import Token
 
 from .models import ContactUs
-@receiver(post_save, sender=ContactUs)
-def SendEmailContactUs(sender, instance=None, created=False, **kwargs):
-    if created:
-        msg = EmailMultiAlternatives(instance.subject, instance.message, instance.contact_email, ['contact@callnmail.com',])
-        msg.send()
 
 
-@receiver(post_save, sender=User)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-        UserProfile.objects.create(user=instance)
+
 
