f7d3ed00932077ac3fe910aae3f44a69def71f96
Mohammed Alhajeri <malhajri@ooredoo.com.kw>
2015-12-21 17:10:26 +0300

    adding admin

--- callmail_project/mail/admin.py
+++ callmail_project/mail/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 from .models import Mail, MailAttachment, MailForward, ForgotPasswordToken, AccountActivation, ContactUs
 
+class MailForwardAdmin(admin.ModelAdmin):
+    list_display = ("email","user.username")
+    search_fields = ("email","user.username")
+
+
 #admin.site.register(Mail)
 #admin.site.register(MailAttachment)
-admin.site.register(MailForward)
-admin.site.register(ForgotPasswordToken)
+admin.site.register(MailForward,MailForwardAdmin)
+#admin.site.register(ForgotPasswordToken)
 #admin.site.register(AccountActivation)
 admin.site.register(ContactUs)
